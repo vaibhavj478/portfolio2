84ec16d87a8d18f126c2fc114b06635262fedd78
Vaibhav Joshi <vaibhavj478@gmail.com>
2022-08-19 16:24:58 +0530

    init

---
+++ src/components/Home.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+   
+
+    <>
+    <div className='headingContainer' > <h2></h2></div>
+    <div  className='container' >
+
+          <div className='leftContainer' ></div>
+          <div className='rightContainer' ></div>
+
+
+    </div>
+    
+    </>
+  )
+}
+
+export default Home
---
+++ src/components/Project.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Project = () => {
+  return (
+    <>
+    <div className='headingContainer' > <h2></h2></div>
+    <div  className='container' >
+
+          <div className='leftContainer' ></div>
+          <div className='rightContainer' ></div>
+
+
+    </div>
+    
+    </>
+  )
+}
+
+export default Project
